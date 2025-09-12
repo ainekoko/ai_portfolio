36c6ae2e02060890ded0b532de5ffa974a69677f
eba Ai <aitune0413@gmail.com>
2025-09-12 13:19:49 +0900

    feature/フォルダ改修作業

--- ai_portfolio/src/components/sections/ProfileSection.tsx
+++ ai_portfolio/src/components/sections/ProfileSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SectionIdProps } from '@/types/component';
-import SectionTitle from '../atoms/SectionTitle';
+import SectionTitle from '../common/SectionHeader';
 import ContactButtons from '../profile/ContactButtons';
 import { CONTACT_BUTTONS, PROFILE_DATA } from '@/utils/profileData';
 import ProfileInfoTable from '../profile/ProfileInfoTable';
@@ -25,11 +25,7 @@ const ProfileSection = (props: SectionIdProps) => {
 
         <div className='absolute right-8 top-40 opacity-80'>
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src='../assets/images/hokkaido.png'
-            alt='北海道イラスト'
-            className=''
-          />
+          <img src='../assets/images/hokkaido.png' alt='北海道イラスト' />
         </div>
       </div>
     </section>
