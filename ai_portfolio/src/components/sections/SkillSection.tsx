abcbb7280cafd8aa75e08d9dc8ad8866caf8f726
ainekoko <37547570+ainekoko@users.noreply.github.com>
2025-09-20 04:23:10 +0900

    [機能追加] スキルsectionの改修作業 (#12)
    * feature/スキルsectionの改修

---
+++ ai_portfolio/src/components/sections/SkillSection.tsx
@@ -0,0 +1,57 @@
+import { SectionProps } from '@/types/component';
+import React from 'react';
+import SectionHeader from '../common/SectionHeader';
+import SkillItemList from '../skill/SkillItemList';
+import { DOCUMENT_SKILL, SKILL_DATA } from '@/utils/skillData';
+import DocumentSkill from '../skill/SkillDocument';
+import SkillAccordion from '../skill/SkillAccordion';
+
+/**
+ * スキルセクションコンポーネント
+ * @param  isVisible: セクションの表示状態を判定する関数
+ */
+const SkillSection = ({ isVisible }: SectionProps) => {
+  return (
+    <section
+      id='skill'
+      className='bg-[#f8fdfa] relative top-[360vh] w-screen h-screen p-8  mt-8 py-6'
+    >
+      {/* Section Title */}
+      <SectionHeader
+        isVisible={isVisible('skill')}
+        title='skill'
+        subtitle='現場で経験したスキルを一覧化しました'
+      />
+
+      {/* 一つのコンテナで4つのスキルセクションを2x2のグリッドで配置 */}
+      <div className='container grid grid-cols-2'>
+        {/*-- プログラミング言語 -*/}
+        <SkillItemList
+          id='programmingLanguage'
+          title='Programming Language'
+          skillData={SKILL_DATA.programmingLanguage}
+        />
+        {/*-- CMS -*/}
+        <SkillItemList id='cms' title='CMS' skillData={SKILL_DATA.cms} />
+        {/*-- Framework -*/}
+        <SkillItemList
+          id='framework'
+          title='Framework'
+          skillData={SKILL_DATA.framework}
+        />
+        {/*-- Library -*/}
+        <SkillItemList
+          id='library'
+          title='Library'
+          skillData={SKILL_DATA.Library}
+        />
+      </div>
+
+      {/*-- Accordion -*/}
+      <SkillAccordion id='another' skillData={SKILL_DATA.another} />
+      {/*-- 設計書関連 -*/}
+      <DocumentSkill documentData={DOCUMENT_SKILL} />
+    </section>
+  );
+};
+export default SkillSection;
---
+++ ai_portfolio/src/utils/skillData.tsx
@@ -0,0 +1,162 @@
+export const SKILL_DATA = {
+  programmingLanguage: [
+    {
+      label: 'HTML5',
+      img: '/assets/images/icons8-html.svg',
+      year: 8,
+    },
+    {
+      label: 'CSS',
+      img: '/assets/images/icons8-css.svg',
+      year: 8,
+    },
+    {
+      label: 'JavaScript',
+      img: '/assets/images/icons8-javascript.svg',
+      year: 6,
+    },
+    {
+      label: 'TypeScript',
+      img: '/assets/images/icons8-ts.svg',
+      year: 4,
+    },
+    {
+      label: 'PHP',
+      img: '/assets/images/icons8-php.svg',
+      year: 1,
+    },
+  ],
+  cms: [
+    {
+      label: 'WordPress',
+      img: '/assets/images/icons8-wp.svg',
+      year: 2,
+    },
+  ],
+  framework: [
+    {
+      label: 'Next.js',
+      img: '/assets/images/icons8-nextjs.svg',
+      year: 1.6,
+    },
+    {
+      label: 'Vue.js',
+      img: '/assets/images/icons8-vue.svg',
+      year: 2,
+    },
+    {
+      label: 'Laravel',
+      img: '/assets/images/laravel.svg',
+      year: 0.6,
+    },
+    {
+      label: 'Bootstrap',
+      img: '/assets/images/icons8-bootstrap.svg',
+      year: 0.6,
+    },
+    {
+      label: 'Jest',
+      img: '/assets/images/jest.svg',
+      year: 0.6,
+    },
+  ],
+  Library: [
+    {
+      label: 'D3.js',
+      img: '/assets/images/d3.svg',
+      year: 2,
+    },
+    {
+      label: 'Three.js',
+      img: '/assets/images/three.svg',
+      year: 0.6,
+    },
+    {
+      label: 'React',
+      img: '/assets/images/icons8-react-native.svg',
+      year: 1,
+    },
+    {
+      label: 'jQuery',
+      img: '/assets/images/icons8-jquery.svg',
+      year: 2,
+    },
+  ],
+  another: [
+    {
+      label: 'Adobe XD',
+      img: '/assets/images/icons8-adobe-xd.svg',
+      year: 3,
+    },
+    {
+      label: 'Adobe Photoshop',
+      img: '/assets/images/icons8-adobe-photoshop.svg',
+      year: 1,
+    },
+    {
+      label: 'Adobe Illustrator',
+      img: '/assets/images/icons8-adobe-illustrator.svg',
+      year: 1,
+    },
+    {
+      label: 'Figma',
+      img: '/assets/images/icons8-figma.svg',
+      year: 0.6,
+    },
+    {
+      label: 'Visual Studio Code',
+      img: '/assets/images/icons8-visual-studio.svg',
+      year: 6,
+    },
+    {
+      label: 'Flourish',
+      img: '/assets/images/flourish.svg',
+      year: 1,
+    },
+    {
+      label: 'Storybook',
+      img: '/assets/images/icons8-storybook.svg',
+      year: 2,
+    },
+    {
+      label: 'GitHub',
+      img: '/assets/images/icons8-github.svg',
+      year: 2,
+    },
+    {
+      label: 'GitLab',
+      img: '/assets/images/icons8-gitlab.svg',
+      year: 2,
+    },
+    {
+      label: 'Sass',
+      img: '/assets/images/icons8-sass.svg',
+      year: 2,
+    },
+    {
+      label: 'Firebase',
+      img: '/assets/images/icons8-firebase.svg',
+      year: 0.6,
+    },
+    {
+      label: 'Docker',
+      img: '/assets/images/icons8-docker.svg',
+      year: 1,
+    },
+  ],
+};
+
+export const DOCUMENT_SKILL = [
+  {
+    title: '要件概要書',
+    description: 'QAの資料作成等',
+  },
+  {
+    title: '基本設計書',
+    description: '機能書、業務フロー図、外部設計書、WF図',
+  },
+  {
+    title: '詳細設計書',
+    description: '画面遷移図、APIとの整合性確認、内部設計書、テストケースなど',
+  },
+];
