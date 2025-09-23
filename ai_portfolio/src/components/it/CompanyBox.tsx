cca7a9b522ac3599e0d728cdda683e65965a938d
eba Ai <aitune0413@gmail.com>
2025-09-23 21:34:01 +0900

    feature/改修中

--- ai_portfolio/src/components/it/CompanyBox.tsx
+++ ai_portfolio/src/components/it/CompanyBox.tsx
@@ -12,7 +12,6 @@ const CompanyBox = ({
 }: {
   companyData: ItJobIntroductionRightItem[];
 }) => {
-  console.log(companyData);
   return (
     <>
       {companyData.map((data, index: number) => (
