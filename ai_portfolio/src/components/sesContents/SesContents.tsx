c4ca0fd79cac242fc2f813a4c7a051d1ba95f5c3
eba Ai <aitune0413@gmail.com>
2025-10-16 05:34:21 +0900

    feature/SES経歴ページの作成

--- ai_portfolio/src/components/sesContents/SesContents.tsx
+++ ai_portfolio/src/components/sesContents/SesContents.tsx
@@ -81,79 +81,87 @@ const SesContents: React.FC<PropType> = (props) => {
             {ITJOB_INTRODUCTION.map((content, index) => (
               <div
                 key={index}
-                className='border border-gray-300 p-8 md:p-12 mb-12 relative'
+                className='embla__slide lg:flex-[0_0_100%] mb-8 lg:mb-0'
               >
-                {/* <!-- 番号 --> */}
-                <div className='absolute top-8 right-8 text-9xl font-light text-gray-300 leading-none'>
-                  0{index + 1}
-                </div>
-
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
-                  {/* <!-- 左側 --> */}
-                  <div className='space-y-6'>
-                    <div className='flex mb-2'>
-                      <div className='w-24 font-light text-gray-600'>業種</div>
-                      <div className='flex-1'>{content.outsourcedCompany}</div>
-                    </div>
-
-                    <div className='flex mb-2'>
-                      <div className='w-24 font-light text-gray-600'>規模</div>
-                      <div className='flex-1'>{content.scale}</div>
-                    </div>
-
-                    <div className='flex mb-2'>
-                      <div className='w-24 font-light text-gray-600'>
-                        参画期間
-                      </div>
-                      <div className='flex-1'>{content.period}</div>
-                    </div>
-
-                    <div className='flex mb-2'>
-                      <div className='w-24 font-light text-gray-600'>
-                        業務内容
-                      </div>
-                      <div>
-                        <div className='mb-3'>{content.phase.join('/')}</div>
-                        <div className='flex-1' key={index}>
-                          {content.bussinessContent.map((item, i) => (
-                            <div key={i}>・{item}</div>
-                          ))}
-                        </div>
-                      </div>
-                    </div>
-                    <hr className='h-px bg-gradient-to-r from-transparent via-[#ccc] to-transparent border-none' />
-
-                    <div>
-                      <div className='font-light mb-4'>開発環境</div>
-                      <div className='space-y-2 text-sm'>
-                        <div>
-                          <span className='font-medium'>【言語】</span>
-                          HTML/CSS/PHP
-                        </div>
-                        <div>
-                          <span className='font-medium'>【OS】</span> Windows
-                        </div>
-                        <div>
-                          <span className='font-medium'>【FW】</span> Laravel
-                        </div>
-                        <div>
-                          <span className='font-medium'>【ツール】</span>{' '}
-                          Chatwork/GitHub
-                        </div>
-                      </div>
-                    </div>
+                <div className='border border-gray-300 p-2 md:p-8 mb-9 mt-8 relative'>
+                  {/* <!-- 番号 --> */}
+                  <div className='absolute top-8 right-8 text-9xl font-light text-gray-300 leading-none'>
+                    0{index + 1}
                   </div>
 
-                  {/* <!-- 右側 --> */}
-                  <div className='space-y-6 m-auto w-4/5'>
-                    <div className='leading-relaxed text-gray-700'>
-                      {content.content}
+                  <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
+                    {/* <!-- 左側 --> */}
+                    <div className='space-y-6'>
+                      <div className='flex mb-2'>
+                        <div className='w-24 font-light text-gray-600'>
+                          業種
+                        </div>
+                        <div className='flex-1'>
+                          {content.outsourcedCompany}
+                        </div>
+                      </div>
+
+                      <div className='flex mb-2'>
+                        <div className='w-24 font-light text-gray-600'>
+                          規模
+                        </div>
+                        <div className='flex-1'>{content.scale}</div>
+                      </div>
+
+                      <div className='flex mb-2'>
+                        <div className='w-24 font-light text-gray-600'>
+                          参画期間
+                        </div>
+                        <div className='flex-1'>{content.period}</div>
+                      </div>
+
+                      <div className='flex mb-2'>
+                        <div className='w-24 font-light text-gray-600'>
+                          業務内容
+                        </div>
+                        <div>
+                          <div className='mb-3'>{content.phase.join('/')}</div>
+                          <div className='flex-1'>
+                            {content.bussinessContent.map((item, i) => (
+                              <div key={i}>・{item}</div>
+                            ))}
+                          </div>
+                        </div>
+                      </div>
+                      <hr className='h-px bg-gradient-to-r from-transparent via-[#ccc] to-transparent border-none' />
+
+                      <div>
+                        <div className='font-light mb-4'>開発環境</div>
+                        <div className='space-y-2 text-sm'>
+                          <div>
+                            <span className='font-medium'>【言語】</span>
+                            HTML/CSS/PHP
+                          </div>
+                          <div>
+                            <span className='font-medium'>【OS】</span> Windows
+                          </div>
+                          <div>
+                            <span className='font-medium'>【FW】</span> Laravel
+                          </div>
+                          <div>
+                            <span className='font-medium'>【ツール】</span>{' '}
+                            Chatwork/GitHub
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* <!-- 右側 --> */}
+                    <div className='space-y-6 m-auto w-4/5'>
+                      <div className='leading-relaxed text-gray-700'>
+                        {content.content}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             ))}
-          </div>
+          </div>{' '}
         </div>
         {/* Embla Carousel Controls - lg以上のみ表示 */}
         <div className='embla__controls pb-6 lg:grid hidden '>
