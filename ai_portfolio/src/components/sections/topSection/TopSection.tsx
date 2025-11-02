c3dc5638d9ba079ca926c5b80cab737e914bdb27
eba Ai <aitune0413@gmail.com>
2025-11-03 00:04:53 +0900

    feature/Topsection内の改修作業

--- ai_portfolio/src/components/sections/topSection/TopSection.tsx
+++ ai_portfolio/src/components/sections/topSection/TopSection.tsx
@@ -1,68 +1,40 @@
 import { SectionIdProps } from '@/types/component';
 import FadeInElement from '../../common/FadeIn';
 import Image from 'next/image';
+import { BACK_IMG } from '@/utils/TopData';
 /**
  * TopSection.tsx
  * 最初のセクションを表示するコンポーネント
  * @param props - isVisible: boolean
  */
 const TopSection = (props: SectionIdProps) => {
-  const backImg = [
-    {
-      src: '/assets/images/top_1.png',
-      alt: '長女',
-      width: 450,
-      height: 500,
-    },
-    {
-      src: '/assets/images/top_2.jpg',
-      alt: '次女',
-      width: 550,
-      height: 400,
-    },
-    {
-      src: '/assets/images/top_3.jpg',
-      alt: '公園',
-      width: 300,
-      height: 400,
-    },
-    {
-      src: '/assets/images/top_4.png',
-      alt: '花壁',
-      width: 200,
-      height: 400,
-    },
-    {
-      src: '/assets/images/top_5.jpg',
-      alt: 'ネイル',
-      width: 250,
-      height: 200,
-    },
-  ];
-
   return (
-    <section id='topSection' className='w-screen'>
+    <section
+      id='topSection'
+      className='w-screen overflow-hidden'
+      aria-label='トップセクション'
+    >
       <div className='relative h-[1300px]  max-sm:h-[calc(130vw*500/450+135vw*400/550)] w-auto lg:w-[1025px] m-auto'>
         <FadeInElement direction='up' delay={0}>
           <div className='absolute left-0 sm:top-20 top-0   '>
             <Image
-              src={backImg[0].src}
-              alt={backImg[0].alt}
+              src={BACK_IMG[0].src}
+              alt={BACK_IMG[0].alt}
+              width={BACK_IMG[0].width}
+              height={BACK_IMG[0].height}
               className=' shadow-xl max-sm:w-[640px]'
-              width={backImg[0].width}
-              height={backImg[0].height}
             />
           </div>
         </FadeInElement>
 
         <FadeInElement direction='up' delay={0.5}>
-          <div className='absolute right-0 sm:top-[600px] max-sm:left-0 max-sm:top-[calc(135vw*500/450)]'>
+          <div className='absolute left-0 sm:left-auto sm:right-0 top-[calc(100vw*500/450)] sm:top-[600px] w-full sm:w-auto'>
             <Image
-              src={backImg[1].src}
-              alt={backImg[1].alt}
+              src={BACK_IMG[1].src}
+              alt={BACK_IMG[1].alt}
+              width={BACK_IMG[1].width}
+              height={BACK_IMG[1].height}
               className='shadow-xl  max-sm:w-[640px]'
-              width={backImg[1].width}
-              height={backImg[1].height}
             />
           </div>
         </FadeInElement>
@@ -80,7 +52,6 @@ const TopSection = (props: SectionIdProps) => {
           Ai&rsquo;s Portfolio
         </h1>
       </div>
-
       <div
         id='hello'
         className='h-[1100px] w-auto lg:w-[1025px] m-auto relative'
@@ -88,29 +59,29 @@ const TopSection = (props: SectionIdProps) => {
         <FadeInElement direction='up'>
           <div className='absolute top-0 max-sm:hidden'>
             <Image
-              src={backImg[2].src}
-              alt={backImg[2].alt}
+              src={BACK_IMG[2].src}
+              alt={BACK_IMG[2].alt}
+              width={BACK_IMG[2].width}
+              height={BACK_IMG[2].height}
               className='shadow-xl'
-              width={backImg[2].width}
-              height={backImg[2].height}
             />
           </div>
           <div className='absolute left-1/2 -translate-x-1/2  top-44'>
             <Image
-              src={backImg[3].src}
-              alt={backImg[3].alt}
+              src={BACK_IMG[3].src}
+              alt={BACK_IMG[3].alt}
+              width={BACK_IMG[3].width}
+              height={BACK_IMG[3].height}
               className='shadow-xl'
-              width={backImg[3].width}
-              height={backImg[3].height}
             />
           </div>
           <div className='absolute right-0 top-0 max-sm:hidden'>
             <Image
-              src={backImg[4].src}
-              alt={backImg[4].alt}
+              src={BACK_IMG[4].src}
+              alt={BACK_IMG[4].alt}
+              width={BACK_IMG[4].width}
+              height={BACK_IMG[4].height}
               className='shadow-xl'
-              width={backImg[4].width}
-              height={backImg[4].height}
             />
           </div>
         </FadeInElement>
@@ -155,7 +126,6 @@ const TopSection = (props: SectionIdProps) => {
           このサイトで少しでも私の事を知って頂けたら幸いです
         </p>
       </div>
-
       {/* サブテキスト - レスポンシブ対応 */}
     </section>
   );
