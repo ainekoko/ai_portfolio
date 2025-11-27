eec6b76f0bbb9d9cf5875dc73be2f70699097321
eba Ai <aitune0413@gmail.com>
2025-11-27 12:17:06 +0900

    feature/uncoverageの改修

--- ai_portfolio/src/components/common/PageIndicator/PageIndicator.stories.tsx
+++ ai_portfolio/src/components/common/PageIndicator/PageIndicator.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/nextjs';
-import PageIndicator from '@/components/common/pageIndicator/PageIndicator';
+import PageIndicator from './PageIndicator';
 import { useRef } from 'react';
 
 const meta = {
