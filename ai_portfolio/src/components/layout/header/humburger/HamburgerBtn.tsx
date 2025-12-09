ee77e808c1620ddb60baaba3e3e54cdb34c50adb
eba Ai <aitune0413@gmail.com>
2025-12-09 14:55:17 +0900

    feature/不具合改修

--- ai_portfolio/src/components/layout/header/humburger/HamburgerBtn.tsx
+++ ai_portfolio/src/components/layout/header/humburger/HamburgerBtn.tsx
@@ -19,7 +19,6 @@ type HamburgerBtnProps = {
  * @param onClick - クリックハンドラー
  */
 const HamburgerBtn = ({ isOpen, onClick }: HamburgerBtnProps) => {
-  console.log('HamburgerBtn rendered. isOpen:', isOpen);
   return (
     <button
       onClick={onClick}
